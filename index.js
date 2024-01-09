674fa0bf2bd9301c65fd2c3d0f360f2ae9c688e0
Kelechi125 <kelechi125@yahoo.com>
2024-01-08 19:43:00 -0600

    Store elements inside variables and add some event listeners

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteAllbtn = document.querySelector(".footer button");
+
+inputBox.addEventListener("keyup", (event) => {
+    let userEnteredValue = inputBox.value;
+    if (userEnteredValue.trim() != 0) {
+        addBtn.classList.add("active");
+    } else {
+        addBtn.classList.remove("active");
+    }
+})
+
+showTasks();
+
+addBtn.addEventListener("click", () => {
+    let userEnteredValue = inputBox.value;
+    let getLocalStorageData = localStorage.getItem("New Todo");
+    if (getLocalStorageData === null) {
+        listArray = [];
+    } else {
+        listArray = JSON.parse(getLocalStorageData); 
+    }
+    listArray.push(userEnteredValue);
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
+    showTasks();
+    addBtn.classList.remove("active");
+})
